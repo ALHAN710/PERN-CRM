6a5d6a4ef57d959e1643ee14e6211160bf0fc707
ALHAN710 <alhadoumpascal@gmail.com>
2023-01-29 12:42:38 +0100

    Configuration to render the react client application from the backend application

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { customerRouter } from "./customerRoutes";
 import { invoiceRouter } from "./invoiceRoutes";
 import { userRouter } from "./userRoutes";
@@ -8,5 +9,9 @@ const router = express.Router();
 router.use(userRouter);
 router.use(customerRouter);
 router.use(invoiceRouter);
+
+router.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../../client/dist/index.html"))
+})
 
 export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,14 @@ import { NotFoundError } from "./errors/not-found-error";
 import { currentUser } from "./middlewares/current-user";
 import { errorHandler } from "./middlewares/error-handler";
 import router from "./routes";
+import path from "path";
 
 const app = express();
 
 
 // ================================  Middleware section ================================
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+// app.use(express.static(path.join(__dirname, "../../client/dist/assets")));
 
 // ========== Middleware pour la récupération des données au format JSON provenant du Front-End
     // Pour les données directement envoyées du Front-End au format JSON par exple via Axios ou Fetch ou AJAX
